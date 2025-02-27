1ede30f4e418f326de62bcf968909a2b05604444
Niijan Bronx <niijanbronx@Niijans-MacBook-Pro.local>
2025-02-26 19:19:15 -0800

    get func ready for deenie

--- getCityRequest.py
+++ getCityRequest.py
@@ -1,3 +1,4 @@
+import json
 import requests
 import os
 from dotenv import load_dotenv
@@ -17,11 +18,22 @@ def hit_weather_service_api(city):
         print(f"Error: {e}")
 
 
-
 '''Create a function that will take in the res.json() object and convert it to a dictionary, 
 then filter out useless info from the dictionary, convert it back to json and return that.'''
+
+def transform_data(weather_obj_dict):
+    '''Grab the name, country, temp in F, text which is the sunny, cloudy. Create new dict and add those 4 and return it.'''
+    # print(type(weather_obj_dict))
+    # print(weather_obj_dict)
+
+    # write code here
+    pass
+
+
+
 
 
 if __name__ == '__main__':
     city = input("Please give me a city: ")
-    hit_weather_service_api(city)
+    json_weather_obj = hit_weather_service_api(city)
+    transform_data(json_weather_obj)
